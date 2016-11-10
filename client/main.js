41522e7bd03e7e4e107bad0573e87b51a74b4f84
krulik <iwillkissyou@gmail.com>
2016-11-10 16:23:33 +0200

    ajaxify and serialize

--- client/main.js
+++ client/main.js
@@ -1,15 +1,36 @@
 'use strict';
 
-// var baseUrl = 'https://babble-server-1.herokuapp.com';
-var baseUrl = 'http://localhost:9000';
+var forms = document.querySelectorAll('form');
 
-var xhr = new XMLHttpRequest();
-xhr.addEventListener('load', handleResponse);
-xhr.open('GET', baseUrl + '/status');
-xhr.send();
+for (var i = 0; i < forms.length; i++) {
+  ajaxify(forms[i]);
+}
 
-function handleResponse (e) {
-  console.log(e);
-  console.log('Responded on ' + new Date(e.timeStamp));
-  console.log('With data: ' + e.target.responseText);
+function ajaxify (form) {
+  form.addEventListener('submit', onSubmit);
+
+  function onSubmit (e) {
+    e.preventDefault();
+    var action = form.getAttribute('action');
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', handleResponse);
+    xhr.open(form.getAttribute('method') || 'GET', action);
+    xhr.send(serialize(form));
+  }
+
+  function serialize (form) {
+    var serialized = '';
+    for (var i = 0; i < form.elements.length; i++) {
+      var element = form.elements[i];
+      if (element.name) {
+        serialized += element.name + '=' + encodeURIComponent(element.value);
+      }
+    }
+    return serialized;
+  }
+
+  function handleResponse (e) {
+    console.log(e);
+    console.log('Responded with: ' + e.target.responseText);
+  }
 }
